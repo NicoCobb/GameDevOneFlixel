cdeca9e26b60305fc9b2e451d009ebd5460b4caa
JS <shensjw@users.noreply.github.com>
2018-09-25 18:20:22 -0400

    resize buttons

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -20,24 +20,49 @@ class MenuState extends FlxState {
     override public function create(): Void {
         FlxG.mouse.visible = false;
         FlxG.cameras.bgColor = 0xff131c1b;
-        _txtTitle = new FlxText(0, 50, 500, "Project 1", 30);
+        _txtTitle = new FlxText(0, 150, 500, "Project 1", 30);
         _txtTitle.alignment = CENTER;
         _txtTitle.screenCenter(FlxAxes.X);
         add(_txtTitle);
 
-        _btnStart = new FlxButton(0, FlxG.height-200, "Start", clickStart);
+        var yOffset: Int = 250;
+        var btnScale: Int = 2;
+        var labelSize: Int = 16;
+        var btnArray: Array<FlxButton> = [_btnStart, _btnOptions, _btnOptions];
+        _btnStart = new FlxButton(0, FlxG.height-yOffset, "Start", clickStart);
         _btnStart.screenCenter(FlxAxes.X);
-        // _btnStart.scale.set(2,2);
-        // _btnStart.label = _txtTitle;
+        _btnStart.scale.set(2,2);
+        _btnStart.label.size = labelSize;
+        _btnStart.label.alignment = CENTER;
+        _btnStart.label.offset.y += 5;
         add(_btnStart);
 
-        _btnOptions = new FlxButton(0, FlxG.height-150, "Options", clickOptions);
+        yOffset -= 70;
+        _btnOptions = new FlxButton(0, FlxG.height-yOffset, "Options", clickOptions);
         _btnOptions.screenCenter(FlxAxes.X);
+        _btnOptions.scale.set(2,2);
+        _btnOptions.label.size = labelSize;
+        _btnOptions.label.alignment = CENTER;
+        _btnOptions.label.offset.y += 5;
         add(_btnOptions);
 
-        _btnQuit = new FlxButton(0, FlxG.height-100, "Quit", clickQuit);
+        yOffset -= 70;
+        _btnQuit = new FlxButton(0, FlxG.height-yOffset, "Quit", clickQuit);
         _btnQuit.screenCenter(FlxAxes.X);
+        _btnQuit.scale.set(2,2);
+        _btnQuit.label.size = labelSize;
+        _btnQuit.label.alignment = CENTER;
+        _btnQuit.label.offset.y += 5;
         add(_btnQuit);
+
+        // for (i in 0...btnArray.length) {
+        //     btnArray[i].screenCenter(FlxAxes.X);
+        //     btnArray[i].scale.set(btnScale,btnScale);
+        //     btnArray[i].label.size = labelSize;
+        //     btnArray[i].label.alignment = CENTER;
+        //     btnArray[i].label.offset.y += 5;
+        //     // add(btnArray[i]);
+        // }
 
         // Play background music
 		if (FlxG.sound.music == null) {
