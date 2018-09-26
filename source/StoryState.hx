33b9eaf3ca8c0ad02007e7016dab05cb9a923fe4
JS <shensjw@users.noreply.github.com>
2018-09-25 22:08:51 -0400

    change color

--- source/StoryState.hx
+++ source/StoryState.hx
@@ -29,40 +29,42 @@ class StoryState extends FlxState {
 
         FlxG.mouse.visible = false;
         _txtLine1 = new FlxTypeText(80, 100, FlxG.width-120, "There was once a land that was ripe for reptiles and amphibians alike.", 20, true);
-        _txtLine1.color = FlxColor.WHITE;
+        _txtLine1.color = FlxColor.CYAN;
 
         _txtLine2 = new FlxTypeText(80, 40, FlxG.width-120, "A land where the dreams of all the little turtles and salamanders of the world could come to fruition.", 20, true);
         _txtLine2.y = _txtLine1.height + _txtLine1.y + 20;
-        _txtLine2.color = FlxColor.WHITE;
+        _txtLine2.color = FlxColor.CYAN;
 
         _txtLine3 = new FlxTypeText(80, 40, FlxG.width-120, "Unfortunately, it was the dream of one such turtle to drive all the salamanders from this slice of paradise.", 20, true);
         _txtLine3.y = _txtLine2.height*2 + _txtLine2.y + 20;
-        _txtLine3.color = FlxColor.WHITE;
+        _txtLine3.color = FlxColor.CYAN;
 
         _txtLine4 = new FlxTypeText(80, 40, FlxG.width-120, "He'd wander around, dousing the ground with water to keep the salamanders away.", 20, true);
         _txtLine4.y = _txtLine3.height*2 + _txtLine3.y + 20;
-        _txtLine4.color = FlxColor.WHITE;
+        _txtLine4.color = FlxColor.CYAN;
 
         _txtLine5 = new FlxTypeText(80, 40, FlxG.width-120, "He was the turtle the salamanders warned their children about.", 20, true);
         _txtLine5.y = _txtLine4.height + _txtLine4.y + 20;
-        _txtLine5.color = FlxColor.WHITE;
+        _txtLine5.color = FlxColor.CYAN;
 
         _txtLine6 = new FlxTypeText(80, 40, FlxG.width-120, "However, one day a little salamander was born who had a dream of his own: to drive all the turtles away from this land.", 20, true);
         _txtLine6.y = _txtLine5.height + _txtLine5.y + 20;
-        _txtLine6.color = FlxColor.WHITE;
+        _txtLine6.color = FlxColor.CYAN;
 
         _txtLine7 = new FlxTypeText(80, 40, FlxG.width-120, "So this salamander set out with his flames to fight back against the turtle and claim this land for the amphibians once and for all.", 20, true);
         _txtLine7.y = _txtLine6.height*2 + _txtLine6.y + 20;
-        _txtLine7.color = FlxColor.WHITE;
+        _txtLine7.color = FlxColor.CYAN;
 
         _txtNext = new FlxText(0, 0, "Press Enter to Start", 14);
         _txtNext.x = FlxG.width - _txtNext.width - 30;
         _txtNext.y = FlxG.height - _txtNext.height - 10;
         _txtNext.visible = false;
+        _txtNext.color = FlxColor.RED;
 
         _txtSkip = new FlxText(0, 0, "Press Space to Skip", 14);
         _txtSkip.screenCenter(FlxAxes.X);
         _txtSkip.y = FlxG.height - _txtSkip.height - 10;
+        _txtSkip.color = FlxColor.YELLOW;
 
         add(_txtLine1);
         add(_txtLine2);
